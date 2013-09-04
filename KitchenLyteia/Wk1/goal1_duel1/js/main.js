eafb96e169f05fdc567b4c4e66b74c7683c78101
Lyteia Kitchen <lytehighfashion@gmail.com>
2013-09-04 14:01:14 -0400

    start of goal 1 assignmnet 1

---
+++ KitchenLyteia/Wk1/goal1_duel1/js/main.js
@@ -0,0 +1,5 @@
+/*
+Lyteia Kitchen
+September 4,2013
+Duel Battle 1
+*/
---
+++ KitchenLyteia/Wk1/goal1_duel1/main.js
@@ -0,0 +1,10 @@
+/*
+Lyteia Kitchen
+September 4,2013
+Assignment: Goal1: Assignmnet: Duel Battle 1
+*/
+
+//self-executing function
+(function(){
+	
+}());
