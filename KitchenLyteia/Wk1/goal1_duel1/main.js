977d7c373e0e356e1f943d730fffeac7c7c40052
Lyteia Kitchen <lytehighfashion@gmail.com>
2013-09-04 14:12:56 -0400

    variables
    added variables for player health, damage and player name

---
+++ KitchenLyteia/Wk1/goal1_duel1/main.js
@@ -0,0 +1,25 @@
+/*
+Lyteia Kitchen
+September 4,2013
+Assignment: Goal1: Assignmnet: Duel Battle 1
+*/
+
+//self-executing function
+(function(){
+	console.log("FIGHT!!!");
+
+	//Player Name
+var playerOneName = "SpiderMan";
+var playerTwoName = "Batman";
+
+//Player Damage
+var palyerOneDamage = 20;
+var playerTwoDamage = 20;
+
+//Player Health
+var playerOneHealth = 100;
+var playerTwoHealth = 100;
+
+
+}());
+
