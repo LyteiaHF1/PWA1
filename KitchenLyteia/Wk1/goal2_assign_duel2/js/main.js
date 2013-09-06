2a1e7f939c4bef18eabeb0e51537314e549786ae
Lyteia Kitchen <lytehighfashion@gmail.com>
2013-09-05 21:31:13 -0400

    start

--- KitchenLyteia/Wk1/goal2_assign_duel2/js/main.js
+++ KitchenLyteia/Wk1/goal2_assign_duel2/js/main.js
@@ -9,47 +9,58 @@ Assignment: Goal1: Assignmnet: Duel Battle 1
 	console.log("FIGHT!!!");
 
 	//Player Name
-var playerOneName = "SpiderMan";
-var playerTwoName = "Batman";
+//var playerOneName = "SpiderMan";
+//var playerTwoName = "Batman";
 
 //Player Damage
-var playerOneDamage = 20;
-var playerTwoDamage = 20;
+//var playerOneDamage = 20;
+//var playerTwoDamage = 20;
 
 //Player Health
-var playerOneHealth = 100;
-var playerTwoHealth = 100;
+//var playerOneHealth = 100;
+//var playerTwoHealth = 100;
+
+//Using arrays to store player name,damgae and health
+var fighter1 = ["Spiderman",20,100];
+var fighter2 = ["Batman",20,100];
 
 var round = 0;
 
 function fight(){
 	//console.log("In the fight function");
-	alert(playerOneName+ ":"+playerOneHealth+ "*START* "+playerTwoName + ":" + playerTwoHealth)
+	//alert(playerOneName+ ":"+playerOneHealth+ "*START* "+playerTwoName + ":" + playerTwoHealth)
+	alert(fighter1[0]+ ":"+fighter1[2]+ "*START* "+fighter2[0] + ":" + fighter2[2]);
 	for(var i =0; i<10; i++){
 		//console.log(i);
 		//random formula --- Math.floor(Math.random()*(max-min)+ min)
 		var minDamage1 = playerOneDamage *.5;
 		var minDamage2 = playerTwoDamage *.5;
-		var f1 = Math.floor(Math.random()*(playerOneDamage - minDamage1)+minDamage1);
-		var f2 = Math.floor(Math.random()*(playerTwoDamage - minDamage2)+minDamage2);
+		//var f1 = Math.floor(Math.random()*(playerOneDamage - minDamage1)+minDamage1);
+		var f1 = Math.floor(Math.random()*(fighter1[1] - minDamage1)+minDamage1);
+		//var f2 = Math.floor(Math.random()*(playerTwoDamage - minDamage2)+minDamage2);
+		var f2 = Math.floor(Math.random()*(fighter2[1] - minDamage2)+minDamage2);
 
 		//console.log(f1);
 		//console.log(f2);
 
 		//damge
-		playerOneHealth -=f1;
-		playerTwoHealth -=f2;
+		//playerOneHealth -=f1;
+		fighter1[2] -=f1;
+		//playerTwoHealth -=f2;
+		fighter2[2] -=f2;
 
 		//console.log(playerOneHealth);
 		//console.log(playerTwoHealth);
-		console.log(playerOneName+ ":"+playerOneHealth+ " "+playerTwoName + ":" + playerTwoHealth)
+		//console.log(playerOneName+ ":"+playerOneHealth+ " "+playerTwoName + ":" + playerTwoHealth)
+		console.log(fighter1[0]+ ":"+fighter1[2]+ " "+fighter2[0] + ":" + fighter2[2])
+
 		
 		var results = winnerCheck();
 		console.log(results)
 
 		if(results === "no winner"){
 			round++;
-			alert(playerOneName+ ":"+playerOneHealth+ "*ROUND "+round+ "OVER*"+playerTwoName + ":" + playerTwoHealth)
+			alert(fighter1[0]+ ":"+fighter1[2]+ "*ROUND "+round+ "OVER*"+fighter2[0] + ":" + fighter1[2])
 		}else{
 			alert(results);
 			break;
@@ -63,12 +74,12 @@ function winnerCheck(){
 	console.log("In Winner Check");
 	//function checks if there is a winner
 	var result = "no winner";
-	if(playerOneHealth<1 && playerTwoHealth<1){
+	if(fighter1[2]<1 && fighter2[2]<1){
 		result = "You Both Die";
-	}else if(playerOneHealth<1){
-		result = playerTwoName+ " WINS!!!"
-	}else if(playerTwoHealth<1){
-		result = playerOneName+ " WINS!!!"
+	}else if(fighter1[2]<1){
+		result = fighter2[0]+ " WINS!!!"
+	}else if(fighter2[2]<1){
+		result = fighter1[0]+ " WINS!!!"
 	}
 
 	return result;
