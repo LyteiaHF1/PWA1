275ede658a055e3e80e84ca0fb3216fa5257ac54
Lyteia Kitchen <lytehighfashion@gmail.com>
2013-09-12 19:46:06 -0400

    Made Fight Function Work With New Code

--- KitchenLyteia/Wk2/goal4_assign_duel3/js/main.js
+++ KitchenLyteia/Wk2/goal4_assign_duel3/js/main.js
@@ -21,109 +21,63 @@ Assignment: Goal4: Assignmnet: Duel Battle 3
 //var playerTwoHealth = 100;
 
 //Using arrays to store player name,damgae and health
-var fighters = {
-	fighter1:[
-		{
-			name:Spiderman,
-			damage:20,
-			health:100
+var fighters=[
+		fighter1={name:"Spiderman", damage:20, health:100}, 
+		fighter2={name:"Batman", damage:20, health: 100}
+];
 
-		}
-	],
-	fighter2:[
-		{
-			name:Batman,
-			damage:20,
-			health:100
-
-		}
-
-	]
-}
-
-
-/*
-var fighter1 = [
-	{
-	name:Spiderman,
-	damage:20,
-	health:100
-
-	};
-]//Player name, Player Damage, Player Health
-var fighter2 = [
-	{
-	name:Batman,
-	damage:20,
-	health:100
-	};
-]//Player name, Player Damage, Player Health
-*/
 var round = 0;
 
-function fight(){
-	//console.log("In the fight function");
-	//alert(playerOneName+ ":"+playerOneHealth+ "*START* "+playerTwoName + ":" + playerTwoHealth)
-	alert(fighter1.name+ ":"+fighter1.health+ "*START* "+fighter2.name + ":" + fighter2.health);
-	for(var i =0; i<10; i++){
-		//console.log(i);
-		//random formula --- Math.floor(Math.random()*(max-min)+ min)
-		//var minDamage1 = playerOneDamage *.5;
-		var minDamage1 = fighter1.damage *.5;
-		//var minDamage2 = playerTwoDamage *.5;
-		var minDamage2 = fighter2.damage *.5;
+document.getElementById("fight_btn").onclick = function fight(){
+		var fighter1minDam=fighters[0].damage*.5;
+		var fighter2minDam=fighters[1].damage*.5;
+		var f1=Math.floor(Math.random()*(fighters[0].damage-fighter1minDam)+fighter1minDam);
+		var f2=Math.floor(Math.random()*(fighters[1].damage-fighter2minDam)+fighter2minDam);
 
-		//var f1 = Math.floor(Math.random()*(playerOneDamage - minDamage1)+minDamage1);
-		var f1 = Math.floor(Math.random()*(fighter1.damage - minDamage1)+minDamage1);//fighter1[1] the # in the brackets is the place in the array where that value is arrays start at 0
-		//var f2 = Math.floor(Math.random()*(playerTwoDamage - minDamage2)+minDamage2);
-		var f2 = Math.floor(Math.random()*(fighter2.damage - minDamage2)+minDamage2);//fighter2[1] the # in the brackets is the place in the array where that value is arrays start at 0
+		//inflict damage
+		fighters[0].health-=f1;
+		fighters[1].health-=f2;
 
-		//console.log(f1);
-		//console.log(f2);
+		console.log(fighters[0].name+";"+fighters[0].health+" "+fighters[1].name+";"+fighters[1].health);
 
-		//damge
-		//playerOneHealth -=f1;
-		fighter1.health -=f1;//fighter1 [2] health is the third value in the array which makes it 2
-		//playerTwoHealth -=f2;
-		fighter2.health -=f2;
-
-		//console.log(playerOneHealth);
-		//console.log(playerTwoHealth);
-		//console.log(playerOneName+ ":"+playerOneHealth+ " "+playerTwoName + ":" + playerTwoHealth)
-		console.log(fighter1.name+ ":"+fighter1.health+ " "+fighter2.name + ":" + fighter2.health)
-
-		
 		var results = winnerCheck();
-		console.log(results)
+		console.log(results);
 
-		if(results === "no winner"){
+		if(results==="no winner"){
 			round++;
-			alert(fighter1.name+ ":"+fighter1.health+ "*ROUND "+round+ "OVER*"+fighter2.name + ":" + fighter2.health)
-		}else{
-			alert(results);
-			break;
+			document.querySelector("#kabal p").innerHTML = fighters[0].health+" <br/> "+fighters[0].name;
+			document.querySelector("#kratos p").innerHTML = fighters[1].health+" <br/> "+fighters[1].name;
+			document.getElementById("round").innerHTML = "Round "+round+" - "+results;
 		}
-
-
+		else{
+			document.querySelector("#kabal p").innerHTML = fighters[0].health+" <br/> "+fighters[0].name;
+			document.querySelector("#kratos p").innerHTML = fighters[1].health+" <br/> "+fighters[1].name;
+			document.getElementById("round").innerHTML = "Round "+round+" - "+results;
+			document.getElementById("fight_btn").onclick = "event.cancelBubble = true";
+			document.querySelector(".buttonblue").innerHTML = "RESET";
+			document.querySelector("#fight_btn .buttonblue").onclick = function reset(){
+				window.location.reload();
+			}
+		}
 	}
-	
-}
+
 function winnerCheck(){
-	console.log("In Winner Check");
-	//function checks if there is a winner
-	var result = "no winner";
-	if(fighter1.health<1 && fighter2.health<1){//if fighter 1 health is less than 1 && fighter2 health is less than 1 they both die
-		result = "You Both Die";
-	}else if(fighter1.health<1){
-		result = fighter2.name+ " WINS!!!"
-	}else if(fighter2.health<1){
-		result = fighter1.name+ " WINS!!!"
-	}
 
-	return result;
-}
+		var result="no winner";
+
+		if(fighters[0].health<1 && fighters[1].health<1){
+			result="You Both Die";
+		}
+		else if (fighters[0].health<1){
+			result=fighters[1].name+" Wins";
+		}
+		else if (fighters[1].health<1){
+			result=fighters[0].name+" Wins";
+		}
+		return result;
+	}
 //program starts here
-fight();
+//fight();
 
 }());
 
