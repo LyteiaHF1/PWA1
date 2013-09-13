c6405820df1840c2fff10e08d4831c0f6c2d76a4
Lyteia Kitchen <lytehighfashion@gmail.com>
2013-09-13 13:21:49 -0400

    Added RegEx

--- KitchenLyteia/Wk2/goal5_assign_validation/js/validate.js
+++ KitchenLyteia/Wk2/goal5_assign_validation/js/validate.js
@@ -7,6 +7,11 @@
 */
 
 (function(){
+    var formData = {
+        userName:document.querySelector("#f_username"),
+        email:document.querySelector("#f_email"),
+        phone:document.querySelector("#f_phone"),
+    }
 
     myform.onsubmit = function(e){
 
@@ -14,6 +19,8 @@
         //You must dynamically retrieve the ID name from the DOM/HTML.
 
         validateField(id);  //id = is the form input field ID
+        validateField(formData.userName);
+        validateField(formData.email);
 
 
         e.preventDefault();
@@ -23,16 +30,28 @@
 
     var validateField = function(inputName){
 
-        if (inputName.name === 'id name goes here'){
-            var pattern = 'a RegEx pattern goes here'
+        if (inputName.name === 'f_username'){
+            var pattern = /^([A-Z][a-z]+)\s([A-Z][a-zA-Z-]+)$/;
 
             //You will need to create an else-if statement for each input field id.  The
             //      format will be similar to the above IF statement.
 
 
-        };
+        }else if(inputName.name === 'f_email'){
+            var pattern = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/;
 
-        var pass = 'the RegEx .test statement is needed here';
+        }else if (inputName.name === 'f_phone'){
+            var pattern = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/;
+
+        }else if(inputName.name === 'f_password'){
+            var pattern = /^[a-zA-Z]\w{3,14}$/;
+
+        }else if(inputName.name === 'f_ssn'){
+            var pattern = /^\d{3}-\d{2}-\d{4}$/; 
+        }
+
+
+        var pass = pattern.test(inputName.value;
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
 
         if (!pass || inputName.value.length < 2){
