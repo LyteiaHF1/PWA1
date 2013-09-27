743dbc0edcd57e3ca6ca67669473d53d6cbaf1b2
Lyteia Kitchen <lytehighfashion@gmail.com>
2013-09-27 09:09:24 -0400

    Last Lab Assignment

--- KitchenLyteia/Wk4/Lab10/js/ball.js
+++ KitchenLyteia/Wk4/Lab10/js/ball.js
@@ -3,6 +3,35 @@
 	September 25,2013
 	Goal 10
 */
-(function (){
+
+function Ball (radius, color){
+
+	// default ball size
+	if(radius === undefined) {radius = 40;}
+	// default color
+	if(color === undefined) {color = "#ff0000";}
+
+	// set ball properties 
+	this.x = 0;
+	this.y = 0; 
+	this.radius = radius;
+	this.vx = 0;
+	this.vy = 0;
+	this.color = color;
+	this.lineWidth = 1;
+};
+
+// basically creates a ball on the canvas
+Ball.prototype.update = function(context){
+
+	context.lineWidth = this.lineWidth;
+	context.fillStyle = this.color;
+	context.beginPath();
+
+	// x, y, radius, start angle, end angle, anti-clockwise
+	context.arc(this.x, this.y, this.radius, 0, (Math.PI * 2), true);
+	context.closePath();
+	context.fill();
+
+};
 	
-})();
--- KitchenLyteia/Wk4/Lab10/js/main.js
+++ KitchenLyteia/Wk4/Lab10/js/main.js
@@ -3,7 +3,77 @@
 	September 25,2013
 	Goal 10
 */
-console.log("Start");
+
+console.log("start chart main.js");
+
 (function (){
-	
+    
+    var canvas = document.createElement("canvas");
+    canvas.width = 640;
+    canvas.height = 480;
+    document.body.appendChild(canvas);
+    var ctx = canvas.getContext("2d");
+    //ctx.fillStyle = "#ccc";
+    //ctx.fillRect(0,0,640,480);
+
+    ball = new Ball(25, "#0000ff"), 
+    vx = 15;
+    vy = 15;
+
+    ball.x = canvas.width * .5;
+    ball.y = canvas.height * .5;
+    
+    // initially add the ball to the canvas
+    ball.update(ctx);
+
+    // var to detect what key has been pressed
+    window.addEventListener('keydown', function (e) {
+
+        // left
+        if (e.keyCode === 37){
+            moveBall("left");
+        }
+        // up
+        if (e.keyCode === 38){
+            moveBall("up");
+        }
+        // right
+        if (e.keyCode === 39){
+            moveBall("right");
+        }
+        // down
+        if (e.keyCode === 40){
+            moveBall("down");
+        }
+
+    }, false);
+    
+     // function to move the ball, pass in the direction it should move
+    function moveBall(dir){
+
+        // clear canvas before drawing every frame
+        ctx.clearRect(0 , 0, canvas.width, canvas.height);
+        var left = 0,
+        right = canvas.width,
+        top = 0,
+        bottom = canvas.height;
+
+        // add or subtract to the x or y based on the direction
+        if(dir === "left"){
+            ball.x -= vx;
+        }
+        else if(dir === "right"){
+            ball.x += vx;
+        }
+        else if(dir === "up"){
+            ball.y -= vx;
+        }
+        else if(dir === "down"){
+            ball.y += vx;
+        }
+
+        // put the ball on the canvas again
+        ball.update(ctx);
+    };
+
 })();
